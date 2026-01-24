da062df3ce5c8062bcec3164531fc191aa774318
AnastasiiaDim <nastasiya.dim@gmail.com>
2026-01-24 21:38:30 +0100

    feat: add solution for Anagram Detection

---
+++ 7-kyu/anagram_detection/solution.py
@@ -0,0 +1,13 @@
+def is_anagram(test, original):
+    return sorted(test.lower()) == sorted(original.lower())
+
+
+
+    # test = test.lower()
+    # original = original.lower()
+    # test_1 = sorted(test)
+    # original_1 = sorted(original)
+    # if test_1 == original_1:
+    #     return True
+    # else:
+    #     return False
