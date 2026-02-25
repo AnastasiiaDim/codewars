7c4742b98b1de88d9a7de174534b351bf650df1e
AnastasiiaDim <nastasiya.dim@gmail.com>
2026-02-25 21:43:19 +0100

    feat: solved task in Sum of Parts

---
+++ 6-kyu/sum_of_parts/solution.py
@@ -0,0 +1,9 @@
+def parts_sums(ls):
+    current_sum = sum(ls)
+    result = [current_sum]
+
+    for num in ls:
+        current_sum = current_sum - num
+        result.append(current_sum)
+
+    return result
