07209aa82058eb679e573b2e522657bc08b3dc1d
AnastasiiaDim <nastasiya.dim@gmail.com>
2026-02-25 21:45:43 +0100

    edit in Sum of Parts

--- 6-kyu/sum_of_parts/solution.py
+++ 6-kyu/sum_of_parts/solution.py
@@ -1,7 +1,7 @@
 def parts_sums(ls):
     current_sum = sum(ls)
     result = [current_sum]
-
+    # result = [sum(ls)]
     for num in ls:
         current_sum = current_sum - num
         result.append(current_sum)
